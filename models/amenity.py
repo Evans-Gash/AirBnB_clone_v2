b75bcd8d0d8511e7185640ba8c38ea2832101fd5
jayricka <jayricka17@gmail.com>
2023-09-21 07:21:30 +0300

    db storage

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,22 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""
+Module containing the Amenity class.
+"""
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.place import Place, place_amenity
+import os
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """
+    Amenity class to store information about amenities.
+    """
+
+    __tablename__ = 'amenities'
+
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship(
+        'Place', secondary=place_amenity, back_populates='amenities')
--- models/user.py
+++ models/user.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-""" User Module for HBNB project """
+"""This is the user module for our application."""
 
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base import Base
+from models.base_model import BaseModel
+from models.place import Place
+from models.review import Review
 
 
 class User(BaseModel, Base):
-    """Representation of a User"""
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'users'
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    """User class for representing user information."""
 
-    def __init__(self, *args, **kwargs):
-        """Initialize a User instance"""
-        super().__init__(*args, **kwargs)
+    __tablename__ = "users"
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
