588d374505091d3c2aa53284026bc2e195501085
jayricka <jayricka17@gmail.com>
2023-09-23 19:07:33 +0300

    Updated user.py with required changes

--- models/user.py
+++ models/user.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python3
-"""This is the user module for our application."""
+"""User module"""
 
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.base import Base
-from models.base_model import BaseModel
-from models.place import Place
-from models.review import Review
-
 
 class User(BaseModel, Base):
-    """User class for representing user information."""
+    """User class for storing user information"""
 
     __tablename__ = "users"
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
