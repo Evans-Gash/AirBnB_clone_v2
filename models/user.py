f700aeb4ae4aca5772163afcbf68657c3120dbf3
jayricka <jayricka17@gmail.com>
2023-09-23 19:17:12 +0300

    Resolved merge conflicts in user.py

--- models/user.py
+++ models/user.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
-"""User module"""
 
+"""This is the user class"""
+
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 class User(BaseModel, Base):
-    """User class for storing user information"""
-
-    __tablename__ = "users"
-
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for your login
+        first_name: first name
+        last_name: last name
+    """
